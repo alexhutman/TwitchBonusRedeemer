f06ddc594268543a4c5af147a991df92640c514d
Alex <alexhutman@gmail.com>
2021-05-24 20:17:44 -0400

    1.0.2 - fixed for new Twitch UI

--- twitch-bonus-redeemer.js
+++ twitch-bonus-redeemer.js
@@ -7,17 +7,17 @@ let inclusiveJitterDiff = maxJitterMs - minJitterMs + 1;
 // Check to see if the button is back after `intervalSecs` number of seconds passes
 function checkButton() {
 	// Array with either 0 elements if the button isn't back, or 1 if it is back
-	let pointsButton = document.getElementsByClassName("tw-button tw-button--success");
+	let pointsButton = document.querySelector('button[aria-label="Claim Bonus"]');
 
 	// If the button is back
-	if (pointsButton.length > 0) {
+	if (pointsButton) {
 		// Introduce a random jitter just in case Twitch is onto us monkaW
 		let randJitter = Math.floor(Math.random() * inclusiveJitterDiff + minJitterMs);
 
 		// Click the button after `randJitter` ms
 		setTimeout(function() {
-			pointsButton[0].click();
-			console.log("Redeemed bonus Twitch points!");
+			pointsButton.click();
+			//console.log("Redeemed bonus Twitch points!");
 		}, randJitter);
 	}
 }
